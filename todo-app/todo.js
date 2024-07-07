6585ff15ce806a5ba16cd56b1a6ad0118baa22bf
Yuvi <bhullar.yuvraj30@gmail.com>
2024-07-08 04:40:47 +0530

    Made todo app

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
-
+const path = require('path');
 const app = express();
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
@@ -98,10 +100,14 @@ app.delete('/todos/:id', function(req, res) {
   });
 });
 
-// for all other routes, return 404
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.use((req, res, next) => {
   res.status(404).send();
 });
+
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
